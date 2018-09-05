6cf4c0ebef8586032ca1ad0cfa70888744099ee7
virginOne <0@DESKTOP-AON2PCD>
2018-09-05 14:43:10 +0800

    swagger2

--- blog-webapp/src/main/java/pri/zjj/blog/Controller/DefaultController.java
+++ blog-webapp/src/main/java/pri/zjj/blog/Controller/DefaultController.java
@@ -1,4 +1,4 @@
-package pri.zjj.blog.controller;
+package pri.zjj.blog.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
